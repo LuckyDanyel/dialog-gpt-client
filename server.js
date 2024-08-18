a3566c15592e4b3e5a148ccbe29829feb38dbb5f
Daniil <tere00000000@gmail.com>
2024-08-18 14:48:31 +0300

    fix

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 import express from 'express';
 import fs from 'fs';
+import 'dotenv/config'
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 
 const app = express();
 const port = 3005;
-
+const domain = process.env.VITE_CHAT_GPT_DOMAIN_CLIENT;
 // API маршрут для отдачи JS файла
 app.get('/dialog/script', (req, res) => {
     try {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = dirname(__filename);
-        const assets = path.join(__dirname, '../dist/assets/js');
+        const assets = path.join(__dirname, '/dist/assets/js');
         const jsFiles = fs.readdirSync(assets);
         const mainFile = jsFiles.find((file) => file.includes('main'));
         if(!mainFile) throw new Error('js файл не был найден');
-        const filePath = path.join(assets, mainFile);
-        res.sendFile(filePath);
+        res.send(`${domain}/assets/js/${mainFile}`);
     } catch (error) {
         res.send('Ошибка').status(500);
     }
 });
 
-// Статические файлы (например, для отдачи HTML)
-app.use(express.static('static'));
+app.use(express.static('dist'));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
-const CHAT_GPT_DOMAIN = import.meta.env.VITE_CHAT_GPT_DOMAIN;
+const CHAT_GPT_DOMAIN = import.meta.env.VITE_CHAT_GPT_DOMAIN_SERVER;
 
 export {
     CHAT_GPT_DOMAIN,
