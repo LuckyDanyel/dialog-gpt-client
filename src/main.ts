1e10112a6d1019b27ba4a2361f54f45119c59e09
Daniil <tere00000000@gmail.com>
2024-09-07 18:16:25 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -19,25 +19,29 @@ const removeStyles = () => {
 const createWidget = (styles: Node | undefined) => {
     const app = document.createElement('div');
     const root = getRoot();
-    root.appendChild(app);
+    root?.appendChild(app);
     if(styles) {
-        root.appendChild(styles)
+        root?.appendChild(styles)
     }
-    document.querySelector('body')?.appendChild(root);
     createApp(App).mount(app);   
 };
 
 const getRoot = () => {
     const root = document.createElement('div');
-    root.setAttribute('id', `${ROOT_APP_ID}`);
-
-    return isDev ? root : root.attachShadow({ mode: 'closed' });
+    if(!isDev) {
+        root.attachShadow({ 'mode': 'open' }).innerHTML = `
+            <div id="${ROOT_APP_ID}"> </div>
+        `;
+    } else {
+        root.setAttribute('id', ROOT_APP_ID);
+    }
+    document.querySelector('body')?.appendChild(root);
+    return isDev ? root : root.shadowRoot?.querySelector(`#${ROOT_APP_ID}`);
 };
 
 try {
     const styles = getStyles();
     createWidget(styles);
-    removeStyles();
 } catch (error) {
     console.log(error);
 }
